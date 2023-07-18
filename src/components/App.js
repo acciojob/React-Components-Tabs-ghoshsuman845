8ee589342ee7c2901fcf03c9f4c0e157490dbf7e
Heman <heman10x@gmail.com>
2023-07-18 14:02:33 +0530

    submitting code-Tue Jul 18 2023 14:02:32 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,37 @@
 
+import React, { useState } from 'react';
+import './App.css';
+
+const TABS = [
+  { id: 'home', label: 'Home' },
+  { id: 'about', label: 'About' },
+  { id: 'features', label: 'Features' },
+];
+
+const App = () => {
+  const [activeTab, setActiveTab] = useState(TABS[0].id);
+
+  const handleTabClick = (tabId) => {
+    setActiveTab(tabId);
+  };
+
+  return (
+    <div className="tabs">
+      {TABS.map((tab) => (
+        <div
+          key={tab.id}
+          className={`tab ${activeTab === tab.id ? 'active' : ''}`}
+          id={`${tab.id}-tab`}
+          onClick={() => handleTabClick(tab.id)}
+        >
+          {tab.label}
+        </div>
+      ))}
+      <div className="viewport">
+        Pages Go Here
+      </div>
+    </div>
+  );
+};
+
+export default App;
